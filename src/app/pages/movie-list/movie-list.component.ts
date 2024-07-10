0d83ec2474adfcf75a8e8d45ef8244935d6f19a2
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-07-10 16:18:07 -0300

    Ajustes

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/Movie';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+
+  movies: Movie[] = [];
+
+  constructor(private service: MovieService){}
+
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  loadMovies(): void {
+    this.service.getMovies().subscribe((movies: Movie[]) => {
+      this.movies = movies;
+    },
+      (error: any) => {
+        console.error('Error fetching movies', error)
+      }
+    )
+  }
 
 }
